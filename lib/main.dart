1abd88ad4b9a066892709d91243a259ce3a99fdd
Sebastian Lech <lechsebastian99@gmail.com>
2024-10-15 00:03:57 +0200

    Change App ID

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        useMaterial3: false,
         primarySwatch: Colors.purple,
       ),
       home: const HomePage(),
